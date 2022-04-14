364093fd1d70374a430c512c6de251f8a2f71850
Kestutis Jakseboga <kestutis.jakseboga@tradimo.com>
2022-04-14 20:37:04 +0300

    url-micro / missed one line in routes

--- url-service/src/routes/UrlRoutes.ts
+++ url-service/src/routes/UrlRoutes.ts
@@ -4,7 +4,7 @@ import UrlController from "../controllers/UrlController.js";
 const urlRoutes = Router();
 
 urlRoutes.get("/api/v1/url", (req, res) => UrlController.listTopUrls(req, res));
-urlRoutes.get("/api/v1/url/:slug", () => UrlController.getUrl);
+urlRoutes.get("/api/v1/url/:slug", (req, res) => UrlController.getUrl);
 urlRoutes.post("/api/v1/url", (req, res) => UrlController.createUrl(req, res));
 
 export default urlRoutes;
